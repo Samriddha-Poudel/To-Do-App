4b11754f08c1ed468e0a9b9ce8c09fa323eca728
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-31 10:56:37 +0545

    added task item section with svg checkmark

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const wrapper=document.querySelector('.wrapper');
+const backBtn=document.querySelector(".back-btn");
 const menubtn=document.querySelector('.menu-btn');
 
 
 menubtn.addEventListener('click', toggleScreen);
+backBtn.addEventListener('click', toggleScreen);
 
 function toggleScreen  ()  {
 wrapper.classList.toggle("show-category");
