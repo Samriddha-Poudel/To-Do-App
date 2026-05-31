4d68e797bc46e52588098ce190b72d7fac6ec835
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-31 13:31:56 +0545

    added toggle funconality for the add task form

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const wrapper=document.querySelector('.wrapper');
-const backBtn=document.querySelector(".back-btn");
-const menubtn=document.querySelector('.menu-btn');
+    const wrapper=document.querySelector('.wrapper');
+    const backBtn=document.querySelector(".back-btn");
+    const menubtn=document.querySelector('.menu-btn');
 
 
-menubtn.addEventListener('click', toggleScreen);
-backBtn.addEventListener('click', toggleScreen);
+    menubtn.addEventListener('click', toggleScreen);
+    backBtn.addEventListener('click', toggleScreen);
 
-function toggleScreen  ()  {
-wrapper.classList.toggle("show-category");
-}
+    function toggleScreen  ()  {
+    wrapper.classList.toggle("show-category");
+    }
+
+    const addTaskbtn= document.querySelector('.add-task-btn');
+    const addTaskform= document.querySelector('.add-task');
+
+
+    const toggleAddtaskForm = () => {
+        addTaskform.classList.toggle('active');
+    };
+
+    addTaskbtn.addEventListener('click', toggleAddtaskForm);
